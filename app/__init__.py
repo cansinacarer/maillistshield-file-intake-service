c70fa05167beec169948f0d3538dde71621fdc90
Cansin Acarer <cansinacarer@users.noreply.github.com>
2024-11-15 01:03:35 -0500

    give cd more time

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,10 @@ from app.uptime import ping_uptime_monitor
 def main():
     # Main loop
     while True:
-        time.sleep(60)
+        start_time = time.time()
         ping_uptime_monitor()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        sleep_time = 60 - elapsed_time
+        if sleep_time > 0:
+            time.sleep(sleep_time)
