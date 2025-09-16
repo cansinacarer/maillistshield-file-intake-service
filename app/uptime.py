4e1f8a5fa9e6888da96f999b1e957ba4f54d50cd
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-09-16 00:29:43 -0400

    feat: log uptime ping errors

--- app/uptime.py
+++ app/uptime.py
@@ -1,10 +1,11 @@
 import requests
 from app.config import UPTIME_MONITOR
+from app.logging import logger
 
 
 # Send a heartbeat the the uptime monitor
 def ping_uptime_monitor():
     try:
         requests.get(UPTIME_MONITOR)
-    except:
-        pass
+    except Exception as e:
+        logger.error(f"Error while sending heartbeat to uptime monitor: {e}")
